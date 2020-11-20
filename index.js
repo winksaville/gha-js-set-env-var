9a06370affbf54ba750750664f5f784f5dbdf9e7
Wink Saville <wink@saville.com>
2020-11-20 11:57:40 -0800

    Fix getinput mis-spelling

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const github = require('@actions/github');
 
 try {
     console.log('index.js:+');
-    const env_name = core.getInpus('env-name');
+    const env_name = core.getInput('env-name');
     console.log(`index.js: env_name=${env_name}`);
-    const env_value = core.getInpus('env-value');
+    const env_value = core.getInput('env-value');
     console.log(`index.js: env_value=${env_value}`);
     core.exportVariable(env_name, env_value);
 } catch (error) {
