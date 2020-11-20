3850c16aba772fd870740ed862cad1aaae81d7d3
Wink Saville <wink@saville.com>
2020-11-20 11:00:18 -0800

    To be consistent sue semi-colons for all lines

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const core = require('@actions/core')
-const github = require('@actions/github')
+const core = require('@actions/core');
+const github = require('@actions/github');
 
 try {
-    console.log('index.js:+')
-    const env_name = core.getInpus('env-name')
+    console.log('index.js:+');
+    const env_name = core.getInpus('env-name');
     console.log(`index.js: env_name=${env_name}`);
-    const env_value = core.getInpus('env-value')
+    const env_value = core.getInpus('env-value');
     console.log(`index.js: env_value=${env_value}`);
-    core.exportVariable(env_name, env_value)
+    core.exportVariable(env_name, env_value);
 } catch (error) {
     core.setFailed(error.message);
 } finally {
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
     console.log(`index.js:- event payload: ${payload}`);
 }
