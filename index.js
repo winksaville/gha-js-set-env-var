1d9cd844a2a5a999b39008a4f585fc69fee4f803
Wink Saville <wink@saville.com>
2020-11-20 12:01:39 -0800

    Throw error if env-name is empty

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const github = require('@actions/github');
 try {
     console.log('index.js:+');
     const env_name = core.getInput('env-name');
+    if(env_name == "") {
+        throw "env-name is empty";
+    }
     console.log(`index.js: env_name=${env_name}`);
     const env_value = core.getInput('env-value');
     console.log(`index.js: env_value=${env_value}`);
