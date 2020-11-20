8daf5e002faa1d71eb54b58b7b4afdc3df3abed3
Wink Saville <wink@saville.com>
2020-11-20 12:09:30 -0800

    Remove console.log messages

--- index.js
+++ index.js
@@ -2,19 +2,12 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-    console.log('index.js:+');
     const env_name = core.getInput('env-name');
     if(env_name == "") {
         throw "env-name is empty";
     }
-    console.log(`index.js: env_name=${env_name}`);
     const env_value = core.getInput('env-value');
-    console.log(`index.js: env_value=${env_value}`);
     core.exportVariable(env_name, env_value);
 } catch (error) {
-    console.log(`index.js: ERROR ${error}`);
     core.setFailed(error);
-} finally {
-    const payload = JSON.stringify(github.context.payload, undefined, 2);
-    console.log(`index.js:- event payload: ${payload}`);
 }
